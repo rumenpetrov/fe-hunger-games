38fd3762c5c23565357f385ef8629880c4bd441e
Rumen Petrov <rpetrov@positivethinking.tech>
2024-01-15 21:08:55 +0200

    Update links to follow the new configuration

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/fe-hunger-games/blog/${post.slug}/`,
 		})),
 	});
 }
