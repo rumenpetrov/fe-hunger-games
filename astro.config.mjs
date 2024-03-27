b127f0dc0a93e7e816cc2169b84288eacf70aff4
Rumen Petrov <rpetrov@positivethinking.tech>
2024-03-27 17:30:30 +0200

    Upgrade dependencies and fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rumenpetrov.github.io',
   base: '/fe-hunger-games',
