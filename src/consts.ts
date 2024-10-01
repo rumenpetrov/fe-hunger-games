4bc60ad8b67662f35b6c7d362751e4389873a247
Rumen Petrov <rumenpetrow@gmail.com>
2024-10-01 09:34:33 +0300

    Make automatic leaderboard calculations

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,21 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'FE hunger games';
-export const SITE_DESCRIPTION = 'Welcome!';
+export const SITE_TITLE = "FE hunger games";
+export const SITE_DESCRIPTION = "Welcome!";
+
+export class LeaderboardEntry {
+  readonly wins: number;
+  readonly mvp: number;
+
+  constructor(props: Record<string, number>) {
+    const { wins, mvp } = props;
+
+    this.wins = wins;
+    this.mvp = mvp;
+  }
+
+  get totalPoints() {
+    return this.wins * 3 + this.mvp * 1;
+  }
+}
